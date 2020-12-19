81173b3d4af7c5bf780bd1ee45ab9831102078f8
Maulana Imam Muttaqin <maulanaimammuttaqin2000@gmail.com>
2020-12-19 12:45:24 +0700

    test bikin navar contrl

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ function removeImage(gambar){
     document.getElementById("main-title").style.display="block";
     document.getElementById(gambar).style.display = "none";
     document.getElementById(gambar).style.animation = "none";
-    document.getElementById("main-title").style.animation = "imageShow .3s ease";
 }
 
-// document.getElementById("main-title").addEventListener('mousemove', (e)=>{
-//     console.log(e.clientX, e.clientY)
-// })
 
 
 window.addEventListener("scroll", function(){
@@ -35,8 +31,8 @@ const checkVis = function(element){
     );
 }
 
-const f1 = document.querySelector("#f1");
-const f2 = document.querySelector("#f2");
+// const f1 = document.querySelector("#f1");
+// const f2 = document.querySelector("#f2");
 // const f3 = document.querySelector("#f3");
 // console.log(document.getElementById("second").getBoundingClientRect())
 // console.log(document.getElementById("third").getBoundingClientRect())
