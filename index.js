dc1d35ce3f002c661814542314702600689dea69
Maulana Imam Muttaqin <maulanaimammuttaqin2000@gmail.com>
2020-12-21 20:32:43 +0700

    selesai navbutton

--- index.js
+++ index.js
@@ -11,80 +11,16 @@ function removeImage(gambar){
 }
 
 function arrowClick(){
-    let el = document.getElementById("arrow");
-    let Ael = document.querySelectorAll(".control a");
-    let mobile = window.matchMedia("(max-width : 480px)");
-    let tablet = window.matchMedia("(max-width : 1040px)");
-
-    
-    let styleArrow = {
-        "transform": "rotate(180deg)",
-        "left": "0.5vw",
-        "boxShadow": "-1px 1px 12px -8px rgba(0,0,0,0.75)",
-        "backgroundColor": "rgb(177, 208, 255)"
-    }
-    let styleArrowRe={
-        "transform": "",
-        "left": "",
-        "boxShadow": "",
-        "backgroundColor": ""
-    }
-    
-    let AelStyle0={
-        "visibility":"visible",
-        "opacity" : "1",
-        "backgroundColor": "rgb(177, 208, 255)",
-        "top":"35%",
-        "left":"5vw"
-    }
-    let AelStyleRe0={
-        "visibility":"hidden",
-        "opacity" : "0",
-        "backgroundColor": "rgb(177, 208, 255)",
-        "top":"40%",
-        "left":"3vw"
-    }
-
-    let AelStyle1={
-        "visibility":"visible",
-        "opacity" : "1",
-        "backgroundColor": "rgb(177, 208, 255)",
-        "top":"50%",
-        "left":"9vw"
-    }
-    let AelStyleRe1={
-        "visibility":"hidden",
-        "opacity" : "0",
-        "backgroundColor": "rgb(177, 208, 255)",
-        "top":"50%",
-        "left":"7vw"
-    }
-
-    let AelStyle2={
-        ...AelStyle0,
-        "top":"65%"
-    }
-    let AelStyleRe2={
-        ...AelStyleRe0,
-        "top":"60%"
-    }
-
-    if(mobile.matches || tablet.matches){
-        AelStyle1["left"] = "20vw"
-        AelStyleRe1["left"] = "19vw"
-    }
-    if(el.style.left === ""){
-        Object.assign(el.style, styleArrow);
-        Object.assign(Ael[0].style, AelStyle0);
-        Object.assign(Ael[1].style, AelStyle1);
-        Object.assign(Ael[2].style, AelStyle2);
+    let navButton = document.querySelector(".control");
+    let arrow = document.getElementById("arrow");
+    if(navButton.style.transform === "rotate(0deg)"){
+        navButton.style.transform = "rotate(-180deg)"
+        arrow.style.transform = "rotate(0deg)"
     }else{
-        Object.assign(el.style, styleArrowRe);
-        Object.assign(Ael[0].style, AelStyleRe0);
-        Object.assign(Ael[1].style, AelStyleRe1);
-        Object.assign(Ael[2].style, AelStyleRe2);
+        navButton.style.transform = "rotate(0deg)"
+        arrow.style.transform = "rotate(180deg)"
     }
-   
+    
 }
 const checkVis = function(element){
     const el = element.getBoundingClientRect()
