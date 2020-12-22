d81e99d6ce02342705237e7280cc2629089f8b83
Maulana Imam Muttaqin <maulanaimammuttaqin2000@gmail.com>
2020-12-22 16:34:04 +0700

    perbaiki index.js

--- index.js
+++ index.js
@@ -55,37 +55,27 @@ function historyAr(dir){
 
 }
 
-let f1 = document.querySelector("#f1"); 
-let f2 = document.querySelector("#f2");
-let f3 = document.querySelector("#f3");
+
+let feature = document.querySelectorAll(".feature");
 let footer = document.querySelector(".logo-footer img");
-let foot = document.querySelector(".footer")
+let foot = document.querySelector(".footer");
 
 document.addEventListener('scroll', function(){
-    if(checkVis(f3)){
-        f3.style.visibility = "visible"
-        f3.style.opacity = "1"
-        f3.style.transform = "scale(1,1)"
-    }else if(checkVis(f2)){
-        f2.style.visibility = "visible"
-        f2.style.opacity = "1"
-        f2.style.transform = "scale(1,1)"
-    }else if(checkVis(f1)){
-        f1.style.visibility = "visible"
-        f1.style.opacity = "1"
-        f1.style.transform = "scale(1,1)"
-    }else if(checkVis(footer)){
+    feature.forEach(f => {
+        if(checkVis(f)){
+            f.style.visibility = "visible"
+            f.style.opacity = "1"
+            f.style.transform = "scale(1,1)"
+        }else{
+            f.style.visibility = "hidden"
+            f.style.opacity = "0"
+            f.style.transform = "scale(.8,.8)"
+        }
+    })
+    if(checkVis(footer)){
         foot.style.clipPath = "polygon(0 100%, 100% 100%, 100% 0, 0 50%)"
     }else{
-        f3.style.visibility = "hidden"
-        f3.style.opacity = "0"
-        f3.style.transform = "scale(.8,.8)"
-        f2.style.visibility = "hidden"
-        f2.style.opacity = "0"
-        f2.style.transform = "scale(.8,.8)"
-        f1.style.visibility = "hidden"
-        f1.style.opacity = "0"
-        f1.style.transform = "scale(.8,.8)"
         foot.style.clipPath = "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)"
     }
+
 })
